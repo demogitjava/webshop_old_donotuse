2dbe320b1509ad0d6f8d77cb7fd50c9090a3267e
hoscho <jgeist@gmx.net>
2022-05-19 16:12:57 +0200

    .

--- src/main/java/de/jgsoftware/webshop/WebshopApplication.java
+++ src/main/java/de/jgsoftware/webshop/WebshopApplication.java
@@ -1,8 +1,14 @@
 package de.jgsoftware.webshop;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.jgsoftware.webshop.iWebShop;
 
 @SpringBootApplication
 public class WebshopApplication implements iWebShop
 {
+
+
+
 }
--- src/main/java/de/jgsoftware/webshop/iWebShop.java
+++ src/main/java/de/jgsoftware/webshop/iWebShop.java
@@ -11,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public interface iWebShop
 {
 
-
     public static void main(String[] args)
     {
         SpringApplication.run(WebshopApplication.class, args);
